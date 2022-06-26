aa2394cf547ad95737f3caf65d61c6d93695d763
Riccardo911 <95646414+Riccardo911@users.noreply.github.com>
2022-06-26 21:30:59 +0200

    home responsiveness - axios delete mypost

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,34 @@
 <template>
   <article>
-     <section >
+     <section class="sticky">
         <div class="create-post-form">
-          <div class="profile-img"></div>
+          <div class="profile-img">
+            <img src="../images/default_user_icon.jpg">
+          </div>
           <div class="nav-user">
             <button @click="newPost">Create post</button>
             <button @click="getPosts()">All posts</button>
             <button>Unread</button>
-            <button @click="myPosts(user)">My posts</button>
+            <button @click="myPosts()">My posts</button>
           </div>
         </div>
     </section>
-    <section v-for="(post, index) in allPosts" v-bind:key="index">
+    <section v-for="(post, index) in allPosts" :key="index">
       <div class="post">
         <div class="post_user">
-          <div class="profile-img"></div>
-          <div class="profile-name">posted by {{ post.userId }}</div>
-          <div class="profile-name">written at {{ post.createdAt }}</div>
+          <div class="profile-img-post">
+            <img src="../images/default_user_icon.jpg">
+          </div>
+          <div>
+            <div class="profile-name">by {{ post.User.firstName }} {{post.User.lastName}}</div>
+            <div class="profile-name">at {{ post.date }}</div>
+          </div>
         </div>
         <div class="post_content">{{post.postText}}</div>
         <div class="post_buttons">
+          <div></div>
           <button>Comment</button>
-          <button>Like</button>
+          <button @click="like(post.post_id)">Like</button>
           <button>Dislike</button>
           <button
           v-if="post.userId == user">
@@ -32,6 +39,7 @@
           @click="deletePost(post.post_id)">
           Delete</button>
         </div>
+        <!-- <div v:bind="user">{{user}}</div> -->
       </div>
     </section> 
   </article>
@@ -43,115 +51,145 @@
 
   export default {
     name: "Home-bar",
-
-    data(){
+    data() {
       return {
-        allPosts:[],
-        user: null
-      }
+        user: null,
+        allPosts: [],
+        allLike: [],
+        allComment: [],
+        dataLike: {
+          // totalLikes:'',
+          userId: "",
+          postId: "",
+          // liked: false
+        },
+        dataLikeString: ""
+      };
     },
-
     methods: {
-
-      /////////////////////////////////////////////////////////////////////
-       async getPosts() {
-        const url = `/post/all/`
-        await axios.get(url, {
-          headers: {
-            Authorization: 'Bearer ' + localStorage.getItem('token')
-          }
-        }).then(response => {
-          let posts = response.data;
-          this.allPosts = posts
-        }).catch(error => { console.log(error)});
-      },
-
-      /////////////////////////////////////////////////////////////////////
-      async myPosts(user) {
-        const url = `/post/all/${user}`
-        await axios.get(url, {
-          headers: {
-            Authorization: 'Bearer ' + localStorage.getItem('token')
-          }
-        }).then(response => {
-          let posts = response.data;
-          this.allPosts = posts
-        }).catch(error => { console.log(error)});
-      },
-
-      /////////////////////////////////////////////////////////////////////
-      newPost() {
-        this.$router.push('home/create') 
-      },
-
-      /////////////////////////////////////////////////////////////////////
-      async deletePost(postId){
-        const url = `/post/all/${postId}`
-
-        //  TODO  : alert 'You want to delete the post?'
-        //        if true -->   DELETE REQUEST
-        
-        await axios.delete( url, { 
-          headers : 
-            { Authorization: 'Bearer ' + localStorage.getItem('token')}
-          }).then((response) => {
-            let res = JSON.parse(response.data)
-            console.log(res)
-            window.location.assign('/home');
-          }).catch((error) => console.log(error.message))
-      },
-
-      /////////////////////////////////////////////////////////////////////
-
+        /////////////////////////////////////////////////////////////////////
+        async like(postId) {
+            this.dataLike.postId = postId;
+            this.dataLike.userId = this.user;
+            // this.dataLikeString = JSON.stringify(this.dataLike)
+            const url = `/post/all/${postId}/like`;
+            await axios.post(url, {
+                userId: parseInt(this.dataLike.userId),
+                postId: this.dataLike.postId
+            }, { headers: {
+                    // 'Content-Type': 'application/json',
+                    Authorization: "Bearer " + localStorage.getItem("token")
+                }
+            }).then(response => {
+                console.log(response);
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        /////////////////////////////////////////////////////////////////////
+        async getPosts() {
+            const url = `/post/all/`;
+            await axios.get(url, {
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("token")
+                }
+            }).then(response => {
+                let posts = response.data;
+                this.allPosts = posts;
+            }).catch(error => { console.log(error); });
+        },
+        /////////////////////////////////////////////////////////////////////
+        async myPosts() {
+            const user = localStorage.userId
+            const url = `/post/all/${user}`;
+            await axios.get(url, {
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("token")
+                }
+            }).then(response => {
+                let posts = response.data;
+                this.allPosts = posts;
+            }).catch(error => { console.log(error); });
+        },
+        /////////////////////////////////////////////////////////////////////
+        newPost() {
+            this.$router.push("home/create");
+        },
+        /////////////////////////////////////////////////////////////////////
+        async deletePost(postId) {
+            const url = `/post/all/${postId}`;
+            //  TODO  : alert 'You want to delete the post?'
+            //        if true -->   DELETE REQUEST
+            await axios.delete(url, {
+                headers: { Authorization: "Bearer " + localStorage.getItem("token") }
+            }).then((response) => {
+                let res = JSON.parse(response.data);
+                console.log(res);
+                window.location.assign("/home");
+            }).catch((error) => console.log(error.message));
+        },
+        /////////////////////////////////////////////////////////////////////
     },
-
     mounted() {
-      //get all post from the DB --> if authorized show posts in user home page
-      axios.get('/post/all', {
-        headers: {
-          Authorization: 'Bearer ' + localStorage.getItem('token')
-        }
-      }).then(response => {
-        let posts = response.data;
-        this.allPosts = posts
-      }).catch(error => { console.log(error)});
-      
-      // user logged
-      this.user = localStorage.getItem('userId');
-    }
-  };
+        //get all post from the DB --> if authorized show posts in user home page
+        axios.get("/post/all", {
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("token")
+            }
+        }).then(response => {
+            let posts = response.data;
+            this.allPosts = posts;
+        }).catch(error => (console.log(error)));
+        // user logged
+        this.user = localStorage.getItem("userId");
+        /////////////////////////////////////////////////////////////////////
+        
+    },
+};
 </script>
 
 <style>
 
-/* section {
-  margin-top:30px;
-} */
+.sticky {
+  position: sticky;
+  top: 65px;
+}
 
 .create-post-form {
-  display: block;
-  border: 1px solid black;
+  /* display: block; */
   min-height: 80px;
   width: 80%;
   margin:auto;
   display:flex;
   border-radius: 10px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  background-color: #333;
+  padding:10px
 }
 
 .profile-img {
-  border: 1px solid red;
+  
   height: 60px;
   width: 60px;
-  margin-left: 10px;
-  margin-top:10px
+  margin:auto
+}
+
+.profile-img img {
+  height: 60px;
+  object-fit: contain
+}
+
+.profile-img-post img {
+  height: 40px;
+  object-fit: contain
 }
 
 .nav-user{
-  /* border: 1px solid black; */
   min-height: 40px;
   width: 85%;
   margin-top:20px;
   margin-left: 20px;
+  margin-bottom:20px;
 }
 
 button {
@@ -177,41 +215,48 @@ button:active {
 .post {
   border-radius: 10px;
   margin:auto;
-  border: 1px solid black;
   min-height: 220px;
   width: 75%;
-  padding: 15px
+  padding: 15px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
 }
 
 .post_user {
   border-radius: 10px;
   margin:auto;
-  border: 1px solid black;
-  height: 60px;
-  display:flex
+  /* border: 1px solid black; */
+  min-height: 60px;
+  display:flex;
+  justify-content: space-between;
+  overflow: hidden;
 }
 
-.post_user .profile-img {
-  border: 1px solid black;
+.post_user .profile-img-post {
   height: 40px;
-  width: 40px;
+  min-width: 40px;
+  margin-left: 10px;
+  margin-top:10px
+
 }
 
 .profile-name {
-  border: 1px solid black;
-  height: 20px;
-  width: 300px; /* max-width?? */
+  /* border: 1px solid black; */
+  height: 28px;
+  min-width: 170px;
   margin:auto;
   margin-left:10px;
-  margin-right:10px
+  margin-right:10px;
+  font-size:initial;
 }
 
 .post_content {
   border-radius: 10px;
   margin:auto;
-  border: 1px solid black;
-  height: 150px;
-  padding:10px
+  /* border: 1px solid black; */
+  min-height: 150px;
+  padding:10px;
+  font-size:large;
+  overflow-wrap: break-word;
 }
 
 .post_buttons{
@@ -242,6 +287,46 @@ button:active {
 
 #delete-button{
   background: red;
+}
+
+@media only screen and (max-width: 1252px) {
+  button {
+    margin-top:10px
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .post_buttons {
+    display:block;
+  }
+}
+
+@media only screen and (max-width: 641px) {
+  .create-post-form {
+    text-align: center
+  }
+}
+
+@media only screen and (max-width: 487px) {
+  .post_buttons {
+    text-align: inherit;
+  }
+}
+
+@media only screen and (max-width: 370px) {
+  .post_buttons {
+    text-align: inherit;
+  }
+
+  button {
+    width:120px;
+  }
+}
+
+@media only screen and (max-width: 339px) {
+  .post_buttons {
+    text-align: center;
+  }
 }
    
 </style>
